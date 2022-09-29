48c51986d20e314e1f3fd9eded72f5fd5874b65d
yyCuanxy <2822843586@qq.com>
2022-09-29 09:57:56 +0800

    feat: 配置标题、serve

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer:{
+    // 端口号
+    port:8888,
+    // 主机名
+    host:"localhost",
+    // 启动项目时，是否自动打开浏览器
+    open:true,
+    // 是否开始https
+    https:false
+
+  }
 });
